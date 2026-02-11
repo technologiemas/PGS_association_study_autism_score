81d6234a75f505afc11db8a9119a3680f9b70f5a
Thomas Sollie <62390942+technologiemas@users.noreply.github.com>
2026-02-11 13:25:06 +0100

    did one more step in measurement invariance analyses after investigating the modification indices

--- scripts/measurement_invariance/extract_data_from_models.R
+++ scripts/measurement_invariance/extract_data_from_models.R
@@ -10,53 +10,68 @@ library(dplyr)
 
 mother_configural = readModels("scripts/measurement_invariance/mother/01_configural_mother.out") 
 mother_configural_extended = readModels("scripts/measurement_invariance/mother/01_configural_mother_extended.out")
+mother_configural_extended_item1 = readModels("scripts/measurement_invariance/mother/01_configural_mother_extended_item1.out")
 father_configural = readModels("scripts/measurement_invariance/father/01_configural_father.out")
 father_configural_extended = readModels("scripts/measurement_invariance/father/01_configural_father_extended.out")
+father_configural_extended_item1 = readModels("scripts/measurement_invariance/father/01_configural_father_extended_item1.out")
 self_configural = readModels("scripts/measurement_invariance/self/01_configural_self.out")
 self_configural_extended = readModels("scripts/measurement_invariance/self/01_configural_self_extended.out")
 self_configural_extended_item1 = readModels("scripts/measurement_invariance/self/01_configural_self_extended_item1.out")
 teacher_configural = readModels("scripts/measurement_invariance/teacher/01_configural_teacher.out")
 teacher_configural_extended = readModels("scripts/measurement_invariance/teacher/01_configural_teacher_extended.out")
+teacher_configural_extended_item1 = readModels("scripts/measurement_invariance/teacher/01_configural_teacher_extended_item1.out")
 
 mother_metric = readModels("scripts/measurement_invariance/mother/02_metric_mother.out")
 mother_metric_extended = readModels("scripts/measurement_invariance/mother/02_metric_mother_extended.out")
+mother_metric_extended_item1 = readModels("scripts/measurement_invariance/mother/02_metric_mother_extended_item1.out")
 father_metric = readModels("scripts/measurement_invariance/father/02_metric_father.out")
 father_metric_extended = readModels("scripts/measurement_invariance/father/02_metric_father_extended.out")
+father_metric_extended_item1 = readModels("scripts/measurement_invariance/father/02_metric_father_extended_item1.out")
 self_metric = readModels("scripts/measurement_invariance/self/02_metric_self.out")
 self_metric_extended = readModels("scripts/measurement_invariance/self/02_metric_self_extended.out")
 self_metric_extended_item1 = readModels("scripts/measurement_invariance/self/02_metric_self_extended_item1.out")
 teacher_metric = readModels("scripts/measurement_invariance/teacher/02_metric_teacher.out")
 teacher_metric_extended = readModels("scripts/measurement_invariance/teacher/02_metric_teacher_extended.out")
+teacher_metric_extended_item1 = readModels("scripts/measurement_invariance/teacher/02_metric_teacher_extended_item1.out")
 
 mother_strong = readModels("scripts/measurement_invariance/mother/03_strong_mother.out")
 mother_strong_extended = readModels("scripts/measurement_invariance/mother/03_strong_mother_extended.out")
+mother_strong_extended_item1 = readModels("scripts/measurement_invariance/mother/03_strong_mother_extended_item1.out")
 father_strong = readModels("scripts/measurement_invariance/father/03_strong_father.out")
 father_strong_extended = readModels("scripts/measurement_invariance/father/03_strong_father_extended.out")
+father_strong_extended_item1 = readModels("scripts/measurement_invariance/father/03_strong_father_extended_item1.out")
 self_strong = readModels("scripts/measurement_invariance/self/03_strong_self.out")
 self_strong_extended = readModels("scripts/measurement_invariance/self/03_strong_self_extended.out")
 self_strong_extended_item1 = readModels("scripts/measurement_invariance/self/03_strong_self_extended_item1.out")
 teacher_strong = readModels("scripts/measurement_invariance/teacher/03_strong_teacher.out")
 teacher_strong_extended = readModels("scripts/measurement_invariance/teacher/03_strong_teacher_extended.out")
+teacher_strong_extended_item1 = readModels("scripts/measurement_invariance/teacher/03_strong_teacher_extended_item1.out")
 
 mother_strict = readModels("scripts/measurement_invariance/mother/04_strict_mother.out")
 mother_strict_extended = readModels("scripts/measurement_invariance/mother/04_strict_mother_extended.out")
+mother_strict_extended_item1 = readModels("scripts/measurement_invariance/mother/04_strict_mother_extended_item1.out")
 father_strict = readModels("scripts/measurement_invariance/father/04_strict_father.out")
 father_strict_extended = readModels("scripts/measurement_invariance/father/04_strict_father_extended.out")
+father_strict_extended_item1 = readModels("scripts/measurement_invariance/father/04_strict_father_extended_item1.out")
 self_strict = readModels("scripts/measurement_invariance/self/04_strict_self.out")
 self_strict_extended = readModels("scripts/measurement_invariance/self/04_strict_self_extended.out")
 self_strict_extended_item1 = readModels("scripts/measurement_invariance/self/04_strict_self_extended_item1.out")
 teacher_strict = readModels("scripts/measurement_invariance/teacher/04_strict_teacher.out")
 teacher_strict_extended = readModels("scripts/measurement_invariance/teacher/04_strict_teacher_extended.out")
+teacher_strict_extended_item1 = readModels("scripts/measurement_invariance/teacher/04_strict_teacher_extended_item1.out")
 
 mother_full = readModels("scripts/measurement_invariance/mother/05_full_mother.out")
 mother_full_extended = readModels("scripts/measurement_invariance/mother/05_full_mother_extended.out")
+mother_full_extended_item1 = readModels("scripts/measurement_invariance/mother/05_full_mother_extended_item1.out")
 father_full = readModels("scripts/measurement_invariance/father/05_full_father.out")
 father_full_extended = readModels("scripts/measurement_invariance/father/05_full_father_extended.out")
+father_full_extended_item1 = readModels("scripts/measurement_invariance/father/05_full_father_extended_item1.out")
 self_full = readModels("scripts/measurement_invariance/self/05_full_self.out")
 self_full_extended = readModels("scripts/measurement_invariance/self/05_full_self_extended.out")
 self_full_extended_item1 = readModels("scripts/measurement_invariance/self/05_full_self_extended_item1.out")
 teacher_full = readModels("scripts/measurement_invariance/teacher/05_full_teacher.out")
 teacher_full_extended = readModels("scripts/measurement_invariance/teacher/05_full_teacher_extended.out")
+teacher_full_extended_item1 = readModels("scripts/measurement_invariance/teacher/05_full_teacher_extended_item1.out")
 
 # -------------------------------------------------------------------------
 # 2. ORGANIZE AND EXTRACT FIT INDICES
@@ -83,13 +98,16 @@ safe_rmsea_str <- function(m) {
 models <- list(
   Mother  = list(mother_configural, mother_metric, mother_strong, mother_strict, mother_full),
   Mother_Extended = list(mother_configural_extended, mother_metric_extended, mother_strong_extended, mother_strict_extended, mother_full_extended),
+  Mother_Extended_Item1 = list(mother_configural_extended_item1, mother_metric_extended_item1, mother_strong_extended_item1, mother_strict_extended_item1, mother_full_extended_item1),
   Father  = list(father_configural, father_metric, father_strong, father_strict, father_full),
   Father_Extended = list(father_configural_extended, father_metric_extended, father_strong_extended, father_strict_extended, father_full_extended),
+  Father_Extended_Item1 = list(father_configural_extended_item1, father_metric_extended_item1, father_strong_extended_item1, father_strict_extended_item1, father_full_extended_item1),
   Self    = list(self_configural, self_metric, self_strong, self_strict, self_full),
   Self_Extended = list(self_configural_extended, self_metric_extended, self_strong_extended, self_strict_extended, self_full_extended),
   Self_Extended_Item1 = list(self_configural_extended_item1, self_metric_extended_item1, self_strong_extended_item1, self_strict_extended_item1, self_full_extended_item1),
   Teacher = list(teacher_configural, teacher_metric, teacher_strong, teacher_strict, teacher_full),
-  Teacher_Extended = list(teacher_configural_extended, teacher_metric_extended, teacher_strong_extended, teacher_strict_extended, teacher_full_extended)
+  Teacher_Extended = list(teacher_configural_extended, teacher_metric_extended, teacher_strong_extended, teacher_strict_extended, teacher_full_extended),
+  Teacher_Extended_Item1 = list(teacher_configural_extended_item1, teacher_metric_extended_item1, teacher_strong_extended_item1, teacher_strict_extended_item1, teacher_full_extended_item1)
 )
 
 extract_metric <- function(lst, field, fun = safe_num) {
@@ -116,11 +134,16 @@ df <- data.frame(
 )
 
 df <- df %>% 
-  rename(`RMSEA (90% CI)` = RMSEA,
-         `DiffTest p-value` = DiffTest_p,
+  rename(`DIFFTEST p-value` = DiffTest_p,
          `Chi-Square` = ChiSq)
 
-write.csv(df, "results/measurement_invariance_results.csv", row.names = FALSE)
+library(openxlsx)
+
+wb <- createWorkbook()
+addWorksheet(wb, "Results")
+addWorksheet(wb, "Modification Indices")
+
+writeData(wb, "Results", df)
 
 # -------------------------------------------------------------------------
 # 3. EXTRACT MODIFICATION INDICES (MEANS/INTERCEPTS/THRESHOLDS)
@@ -180,29 +203,31 @@ all_mi_data <- do.call(rbind, lapply(names(models), function(n) {
   get_mi_details(models[[n]], n)
 }))
 
-# Write the detailed MI results to a separate CSV
-# write.csv(all_mi_data, "results/measurement_invariance_MI_details.csv", row.names = FALSE)
+writeData(wb, "Modification Indices", all_mi_data)
 
-# save as xlsx as well
-library(openxlsx)
-write.xlsx(all_mi_data, "results/measurement_invariance_MI_details.xlsx", rowNames = FALSE)
+note_col_results <- ncol(df) + 2
+note_col_mi <- ncol(all_mi_data) + 2
+note_rows <- 2:11  # Span 10 rows for the note
 
-wb <- loadWorkbook("results/measurement_invariance_MI_details.xlsx")
-sheet_name <- names(wb)[1]
-note_col <- ncol(all_mi_data) + 2
-note_rows <- 1:10  # Span 10 rows for the note
+# Write notes
+writeData(wb, "Results", 
+          "Note: Extended models represent those with modeled covariance between residuals of items. For Mother, Father and Teacher these are item4 WITH item10, and for self this is item4 WITH item9. The DiffTest compare the model against the previous model (i.e. Metric vs Configural). The first p-value under our threshold of p=0.01 in each rater group represents the level of measurement invariance that is not supported by the data.",
+          startRow = note_rows[1], startCol = note_col_results)
 
-# Write note
-writeData(wb, sheet_name, 
+writeData(wb, "Modification Indices", 
           "Note: This file contains detailed Modification Indices (M.I.s) for Means/Intercepts/Thresholds of items extracted from Mplus measurement invariance models. Only M.I.s above 20 from models where the DIFFTEST p-value is below 0.01 are included. Configural and Full models are excluded. \n \"The Std E.P.C. indices are standardized using the variances of the continuous latent variables. The StdYX E.P.C. indices are standardized using the variances of the continuous latent variables as well as the variances of the background and/or outcome variables.\" - (Muthén & Muthén, 1998-2017)",
-          startRow = note_rows[1], startCol = note_col)
+          startRow = note_rows[1], startCol = note_col_mi)
 
 # Merge cells vertically for the note
-mergeCells(wb, sheet_name, cols = note_col, rows = note_rows)
+mergeCells(wb, "Modification Indices", cols = note_col_mi, rows = note_rows)
+mergeCells(wb, "Results", cols = note_col_results, rows = note_rows)
 
 # Apply text wrapping and set column width
 note_style <- createStyle(wrapText = TRUE, valign = "top")
-addStyle(wb, sheet_name, note_style, rows = note_rows, cols = note_col)
-setColWidths(wb, sheet_name, cols = note_col, widths = 50)
+addStyle(wb, "Modification Indices", note_style, rows = note_rows, cols = note_col_mi)
+setColWidths(wb, "Modification Indices", cols = note_col_mi, widths = 50)
 
-saveWorkbook(wb, "results/measurement_invariance_MI_details.xlsx", overwrite = TRUE)
+addStyle(wb, "Results", note_style, rows = note_rows, cols = note_col_results)
+setColWidths(wb, "Results", cols = note_col_results, widths = 50)
+
+saveWorkbook(wb, "results/measurement_invariance_results.xlsx", overwrite = TRUE)
